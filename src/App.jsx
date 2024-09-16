014feeffa0dc993c4340a06cf89b6d4dc5b611d4
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-09-16 13:55:57 +0530

    added a line in app.js file

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div>
       <h1 className="text-yellow-600 text-4xl">Hello CoLearn.</h1>
+      <p>this is CoLearn dashboard from where we can manage our team</p>
     </div>
   );
 }
