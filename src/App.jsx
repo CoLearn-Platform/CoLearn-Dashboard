c746c12361a936daac0f8e07cd3364dfa0426cee
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-09-17 10:31:09 +0530

    setup react-query, supabase, scss and added some UI component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import AppLayout from "./ui/AppLayout";
 import Dashboard from "./pages/Dashboard";
 import NotFound from "./ui/NotFound";
 import AuthForm from "./pages/AuthForm";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    stateTime: 0,
+  },
+});
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route index element={<Navigate to="/dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="auth" element={<AuthForm />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate to="/dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="auth" element={<AuthForm />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,5 +1,51 @@
+import { Link } from "react-router-dom";
+import Button from "./Button";
+import styles from "./Header.module.scss"; // Import the SCSS module
+import { IoIosLogOut } from "react-icons/io";
+
 function Header() {
-  return <div>CoLearn Dashboard</div>;
+  const isAuthenticated = false;
+  const isLoggingOut = false;
+  function handleLogout() {}
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <div className={styles.flexContainer}>
+          {/* Logo */}
+          <div className={styles.logo}>
+            <Link to="/">CoLearn</Link>
+          </div>
+
+          {/* Action Button */}
+          <div className={styles.actionButtons}>
+            {isAuthenticated ? (
+              <>
+                <Link to="/dashboard">
+                  <Button>Dashboard</Button>
+                </Link>
+                <Button
+                  styleType="remove"
+                  onClick={handleLogout}
+                  disabled={isLoggingOut}
+                >
+                  <IoIosLogOut />
+                </Button>
+              </>
+            ) : (
+              <>
+                <Link to="/auth">
+                  <Button styleType="login">Login</Button>
+                </Link>
+                <Link to="/auth">
+                  <Button styleType="primary">Sign Up</Button>
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
